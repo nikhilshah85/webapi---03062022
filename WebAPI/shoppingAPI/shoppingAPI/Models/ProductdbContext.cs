7ce54c8f288de5bde0e22ae17e2330a290c84768
Nikhil <nikhil.shah2008@gmail.com>
2022-06-09 16:35:39 +0530

    API with EF

---
+++ WebAPI/shoppingAPI/shoppingAPI/Models/ProductdbContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace shoppingAPI.Models
+{
+    public partial class ProductdbContext : DbContext
+    {
+        public ProductdbContext()
+        {
+        }
+
+        public ProductdbContext(DbContextOptions<ProductdbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=tcp:shopping-server.database.windows.net,1433;Initial Catalog=Productdb;Persist Security Info=False;User ID=trainer;Password=Password@1234;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(e => e.PId)
+                    .HasName("PK__Products__DD36D562E1966B9A");
+
+                entity.Property(e => e.PId)
+                    .ValueGeneratedNever()
+                    .HasColumnName("pId");
+
+                entity.Property(e => e.PCategory)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("pCategory");
+
+                entity.Property(e => e.PName)
+                    .HasMaxLength(20)
+                    .IsUnicode(false)
+                    .HasColumnName("pName");
+
+                entity.Property(e => e.PPrice).HasColumnName("pPrice");
+
+                entity.Property(e => e.PisInStock).HasColumnName("pisInStock");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
